fed300d58dca5784ac825741bc0657d80c8ce38d
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2024-04-30 16:27:20 +0900

    feat: tsconfig, axios, router

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,11 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      { find: "assets", replacement: "/src/assets" },
+      { find: "pages", replacement: "/src/pages" },
+    ],
+  },
   server: { port: 3000 },
 });
