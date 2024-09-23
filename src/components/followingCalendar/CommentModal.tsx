29486c9f039faa254f46dcfbde2d4c7b465a36a7
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2024-09-23 15:55:50 +0900

    Update CommentModal.tsx

--- src/components/followingCalendar/CommentModal.tsx
+++ src/components/followingCalendar/CommentModal.tsx
@@ -1,5 +1,5 @@
 import { postReaction, ReactionType } from "api/reaction";
-import { FormEvent, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Modal from "react-modal";
 import styled from "styled-components";
 
@@ -17,18 +17,6 @@ const Comment = styled.div`
   justify-content: center;
   gap: 20px;
   overflow-y: hidden;
-`;
-
-const TextForm = styled.form`
-  display: flex;
-  justify-content: center;
-  input {
-    width: 240px;
-    height: 22px;
-    border-radius: 24px;
-    border: 1px solid #dcdcdc;
-    padding: 8px;
-  }
 `;
 
 const EmojiContainer = styled.div`
@@ -62,11 +50,11 @@ export default function CommentModal({
     },
     content: {
       width: window.innerWidth <= 360 ? "100%" : "360px",
-      height: "400px",
+      height: "360px",
       margin: "0 auto",
       padding: "0",
       border: "none",
-      top: "calc(100dvh - 400px)",
+      top: "calc(100dvh - 360px)",
       left: 0,
       right: 0,
       backgroundColor: "white",
@@ -77,7 +65,6 @@ export default function CommentModal({
   });
 
   const [modalStyle, setModalStyle] = useState(getCustomModalStyle());
-  const [comment, setComment] = useState("");
 
   useEffect(() => {
     const handleResize = () => {
@@ -87,22 +74,6 @@ export default function CommentModal({
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
   }, []);
-
-  async function submitComment(event: FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-    const submitText = comment.trim();
-
-    if (submitText) {
-      const response = await postReaction(
-        subGoalId,
-        ReactionType.COMMENT,
-        submitText
-      );
-      console.log(response);
-
-      onClose();
-    }
-  }
 
   async function submitEmoji(codePoint: number) {
     const response = await postReaction(
@@ -119,14 +90,6 @@ export default function CommentModal({
     <>
       <Modal isOpen={isOpen} style={modalStyle} onRequestClose={onClose}>
         <Comment>
-          <TextForm onSubmit={submitComment}>
-            <input
-              type="text"
-              placeholder="메시지 보내기"
-              onChange={(e) => setComment(e.target.value)}
-              value={comment}
-            />
-          </TextForm>
           <EmojiContainer>
             {emojiList.map((codePoint, index) => (
               <Emoji
